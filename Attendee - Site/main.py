c557016203f5fe676f3466d9d08f5625079c25b4
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2023-03-15 22:53:53 +0530

    Sign up page added, static folder added

--- Attendee - Site/main.py
+++ Attendee - Site/main.py
@@ -21,30 +21,44 @@ def home():
     if request.method == "POST":
         loginType = request.form['loginType']
         if loginType == '1':
-            studentDetails = fetchDetails('student_details',request.form['loginId'],request.form['password'])
-            if studentDetails:
-                return render_template('student.html',params=params,loginId=studentDetails[0],name=studentDetails[1],currentDate=datetime.now().date())
+            details = fetchDetails('student_details',request.form['loginId'],request.form['password'])
+            if details:
+                return render_template('student.html',params=params,details=details,currentDate=datetime.now().date())
             else:
-                # TODO
-                print("Invalid credential")
+                return render_template('index.html',params=params,style='d-block',errorMsg='The login id or password you entered is incorrect.')
         elif loginType == '2':
-            facultyDetails = fetchDetails('faculty_details',request.form['loginId'],request.form['password'])
-            if facultyDetails:
-                return render_template('faculty.html',params=params)
+            details = fetchDetails('faculty_details',request.form['loginId'],request.form['password'])
+            if details:
+                print(json.loads(details[-1])[0])
+                return render_template('faculty.html',params=params,details=details,currentDate=datetime.now().date())
             else:
-                # TODO
-                print("Invalid credential")
+                return render_template('index.html',params=params,style='d-block',errorMsg='The login id or password you entered is incorrect.')
         else:
-            # TODO
-            print("Please select a login type")
-    return render_template('index.html',params=params)
+            return render_template('index.html',params=params,style='d-block',errorMsg='Please select a login type')
+    return render_template('index.html',params=params,style='d-none',errorMsg='')
 
 
 @app.route('/about')
 def about():
     return render_template('about.html',params=params)
-
-
+@app.route('/signup',methods=['GET','POST'])
+def signUp():
+    if request.method == "POST":
+        fullName = request.form['firstName'] +" "+ request.form['lastName']
+        email = request.form['email']
+        password = request.form['password']
+        userType = request.form['userType']
+        if userType == '1':
+            department = request.form['department']
+            semester = request.form['semester']
+            rollNumber = request.form['rollNumber']
+            image = request.form['image']
+            print(department,semester,rollNumber,image)
+        elif userType == '2':
+            # TODO
+            print()
+        
+    return render_template('signup.html',params=params)
 @app.route('/contact',methods=['GET','POST'])
 def contact():
     if request.method == "POST":
