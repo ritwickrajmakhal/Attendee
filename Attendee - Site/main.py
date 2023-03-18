51f16651ade3d88dd8e10d8ec13bc039da214d0c
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2023-03-18 20:28:30 +0530

    sign up page connected with database

--- Attendee - Site/main.py
+++ Attendee - Site/main.py
@@ -1,18 +1,44 @@
 from flask import Flask, render_template, request
 import json
 from datetime import datetime
-import methods
+from methods import *
 
 with open('config.json','r') as f:
     params = json.loads(f.read())['params']
     
-def fetchDetails(tableName:str,loginId:str,password:str):
-    sql = f"SELECT * FROM {tableName} WHERE loginId = %s AND password = %s"
-    mycursor.execute(sql,(loginId,password,))
-    details = mycursor.fetchone()
-    return details
-    
-mydb = methods.connectWithServer(params=params)   
+def fetchDetails(userType:str,loginId:str,password:str):
+    mycursor.execute("SHOW TABLES")
+    tables = [table[0] for table in mycursor]
+    for table in tables:
+        if f"{datetime.now().year}" in table:
+            mycursor.execute(f"SELECT password FROM {table} WHERE loginId = %s",(loginId,))
+            result = mycursor.fetchone()
+            if result:
+                hashedPassword = result[0]
+                if checkPassword(password.encode("utf-8"),hashedPassword.encode("utf-8")):
+                    mycursor.execute(f"SELECT * FROM {table} WHERE loginId = %s",(loginId,))
+                    result = mycursor.fetchone()
+                    if userType == 1: # Make json for student
+                        details = {
+                            "loginId" : result[0],
+                            "name" : result[1],
+                            "department" : result[2],
+                            "semester" : result[3],
+                            "image_file" : result[6]                        
+                        }
+                    else: # Make json for faculty
+                        details = {
+                            "loginId" : result[0],
+                            "name" : result[1],
+                            "department" : result[2],
+                            "semester" : result[3],      
+                        }
+                    
+                    return details
+                        
+    return None
+
+mydb = connectWithServer(params=params)   
 
 mycursor = mydb.cursor()
 app = Flask(__name__)
@@ -21,13 +47,13 @@ def home():
     if request.method == "POST":
         loginType = request.form['loginType']
         if loginType == '1':
-            details = fetchDetails('student_details',request.form['loginId'],request.form['password'])
+            details = fetchDetails(loginType,request.form['loginId'],request.form['password'])
             if details:
                 return render_template('student.html',params=params,details=details,currentDate=datetime.now().date())
             else:
                 return render_template('index.html',params=params,style='d-block',errorMsg='The login id or password you entered is incorrect.')
         elif loginType == '2':
-            details = fetchDetails('faculty_details',request.form['loginId'],request.form['password'])
+            details = fetchDetails(loginType,request.form['loginId'],request.form['password'])
             if details:
                 print(json.loads(details[-1])[0])
                 return render_template('faculty.html',params=params,details=details,currentDate=datetime.now().date())
@@ -47,18 +73,44 @@ def signUp():
         fullName = request.form['firstName'] +" "+ request.form['lastName']
         email = request.form['email']
         password = request.form['password']
-        userType = request.form['userType']
-        if userType == '1':
-            department = request.form['department']
-            semester = request.form['semester']
-            rollNumber = request.form['rollNumber']
-            image = request.form['image']
-            print(department,semester,rollNumber,image)
-        elif userType == '2':
-            # TODO
-            print()
-        
-    return render_template('signup.html',params=params)
+        confirm_password = request.form['confirm_password']
+        if checkPassword(confirm_password.encode("utf-8"),encrypt(password)):
+            userType = request.form['userType']
+            if userType == '1':
+                department = request.form['department']
+                semester = request.form['semester']
+                rollNumber = request.form['rollNumber']
+                image_file = request.form['image_file']
+                table_name = f"{department}_{semester}_{datetime.now().year}"
+                try:
+                    sql = f"CREATE TABLE {table_name} (`loginId` BIGINT NOT NULL , `name` TEXT NOT NULL , `department` VARCHAR(10) NOT NULL , `semester` VARCHAR(10) NOT NULL , `email` VARCHAR(50) NOT NULL , `password` VARCHAR(250) NOT NULL , `image_file` VARCHAR(100) NOT NULL )"
+                    mycursor.execute(sql)
+                except:
+                    pass
+                
+                # Check whether roll no exists in a table or not
+                result = fetchDetails(userType, rollNumber,password)
+                if result:
+                    return render_template('thanks-card.html',params=params,message="You've already registered")
+                else:
+                    sql = f"INSERT INTO {table_name} (`loginId`, `name`, `department`, `semester`, `email`, `password`, `image_file`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                    password = encrypt(password)
+                    val = (rollNumber,fullName,department,semester,email, password,image_file)
+                    mycursor.execute(sql,val)
+                    mydb.commit()
+                    return render_template('thanks-card.html',params=params,message="You've registered successfully")
+                
+            elif userType == '2':
+                subject = request.form('subject')
+                table_name = f"{subject}_{datetime.now().year}"
+                print(table_name)
+                
+                # TODO
+            else:
+                return render_template('signup.html',params=params,style='d-block',errorMsg='You haven\'t chosen your role')
+        else:
+            return render_template('signup.html',params=params,style='d-block',errorMsg='Your New password and confirmed password should be same')   
+    return render_template('signup.html',params=params,style='d-none',errorMsg='')
 @app.route('/contact',methods=['GET','POST'])
 def contact():
     if request.method == "POST":
@@ -69,7 +121,7 @@ def contact():
         val = (fullName,email,msg)
         mycursor.execute(sql,val)
         mydb.commit()
-        return render_template('thanks-card.html',params=params)
+        return render_template('thanks-card.html',params=params, message="for getting in touch!")
     else:
         return render_template('contact.html',params=params)
 if __name__ == '__main__':
--- Attendee - Site/methods.py
+++ Attendee - Site/methods.py
@@ -1,20 +1,23 @@
 import mysql.connector
-import bcrypt
 
 def connectWithServer(params):
-    if params['isLocalServer']:
-        localServer = params['localServer']
-        mydb = mysql.connector.connect(host=localServer['host'],
-                                user=localServer['user'],
-                                password=localServer['password'],
-                                database=localServer['database'])
-    else:
-        productionServer = params['productionServer']
-        mydb = mysql.connector.connect(host=productionServer['host'],
-                                user=productionServer['user'],
-                                password=productionServer['password'],
-                                database=productionServer['database'])
-    return mydb
+    try:
+        if params['isLocalServer']:
+            localServer = params['localServer']
+            mydb = mysql.connector.connect(host=localServer['host'],
+                                    user=localServer['user'],
+                                    password=localServer['password'],
+                                    database=localServer['database'])
+        else:
+            productionServer = params['productionServer']
+            mydb = mysql.connector.connect(host=productionServer['host'],
+                                    user=productionServer['user'],
+                                    password=productionServer['password'],
+                                    database=productionServer['database'])
+        return mydb
+    except:
+        print("Turn on your server")
+        return None
 
 def encrypt(password:str):
     import bcrypt
@@ -25,7 +28,6 @@ def encrypt(password:str):
     hashed_password = bcrypt.hashpw(password, salt)
     return hashed_password
 
-def checkPassword(userPassword:str, hashedPassword:bytes):
+def checkPassword(userPassword:str, hashedPassword:str):
     import bcrypt
-    userPassword = userPassword.encode('utf-8')
     return bcrypt.checkpw(userPassword,hashedPassword)
