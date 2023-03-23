bdbce0439899832014e2ed0f67fe74ff8f738bca
ritwickrajmakhal <ritwickrajmakhal11@gmail.com>
2023-03-23 10:09:49 +0530

    Removed bugs, added image uploader

--- Attendee - Site/main.py
+++ Attendee - Site/main.py
@@ -1,116 +1,166 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 import json
 from datetime import datetime
 from methods import *
+import os
 
 with open('config.json','r') as f:
     params = json.loads(f.read())['params']
-    
-def fetchDetails(userType:str,loginId:str,password:str):
-    mycursor.execute("SHOW TABLES")
-    tables = [table[0] for table in mycursor]
-    for table in tables:
-        if f"{datetime.now().year}" in table:
-            mycursor.execute(f"SELECT password FROM {table} WHERE loginId = %s",(loginId,))
-            result = mycursor.fetchone()
-            if result:
-                hashedPassword = result[0]
-                if checkPassword(password.encode("utf-8"),hashedPassword.encode("utf-8")):
-                    mycursor.execute(f"SELECT * FROM {table} WHERE loginId = %s",(loginId,))
-                    result = mycursor.fetchone()
-                    if userType == 1: # Make json for student
-                        details = {
-                            "loginId" : result[0],
-                            "name" : result[1],
-                            "department" : result[2],
-                            "semester" : result[3],
-                            "image_file" : result[6]                        
-                        }
-                    else: # Make json for faculty
-                        details = {
-                            "loginId" : result[0],
-                            "name" : result[1],
-                            "department" : result[2],
-                            "semester" : result[3],      
-                        }
-                    
-                    return details
-                        
-    return None
-
 mydb = connectWithServer(params=params)   
-
 mycursor = mydb.cursor()
 app = Flask(__name__)
+app.secret_key = "Atten-dee"
+app.config['UPLOAD_FOLDER'] = params['upload_location']
+def fetchDetails(userType:str, tables:list, loginId:str, password:str):
+    result = None
+    details = None
+    for table in tables:
+        mycursor.execute(f"SELECT `password` FROM {table} WHERE loginId = %s",(loginId,))
+        result = mycursor.fetchone()
+        if result:
+            if checkPassword(password.encode("utf-8"),result[0].encode("utf-8")):
+                mycursor.execute(f"SELECT * FROM {table} WHERE loginId = %s",(loginId,))
+                result = mycursor.fetchone()
+                if userType == '1':
+                    details = {
+                        "loginId" : result[0],
+                        "name" : result[1],
+                        "department" : result[2],
+                        "semester" : result[3],
+                        "image_file" : result[6],
+                        "date" : datetime.now().date(),
+                        "table_name" : table
+                    }
+                    return details
+                else:
+                    details = {
+                        "loginId" : result[1],
+                        "name" : result[0],
+                        "date" : datetime.now().date()
+                    }
+                    return details         
 @app.route('/',methods=['GET','POST'])
 def home():
-    if request.method == "POST":
+    mycursor.execute("SHOW TABLES")
+    tables = []
+    for table in mycursor:
+        if f"{datetime.now().year}" in table[0]:
+            tables.append(table[0])
+    
+    if 'loginId' in session:
+        loginId = session.get('loginId')
+        loginType = session.get('loginType')
+        password = session.get('password')
+        # validate credentials
+        # make a details dictionary
+        if loginType=='1':
+            details = fetchDetails(loginType, tables,loginId,password)
+            if details:
+                return render_template('student.html',params=params,details=details)
+            else:
+                return render_template('index.html',params=params,error="Please select the user Type or enter the correct user id or password")
+        elif loginType=='2':
+            details = fetchDetails(loginType,['faculty_details'],loginId,password)
+            if details:
+                return render_template('faculty.html',params=params,details=details)
+            else:
+                return render_template('index.html',params=params,error="Please select the user Type or enter the correct user id or password")
+    if request.method == 'POST':
+        loginId = request.form['loginId']
+        password = request.form['password']
         loginType = request.form['loginType']
-        if loginType == '1':
-            details = fetchDetails(loginType,request.form['loginId'],request.form['password'])
+        if loginType=='1':
+            details = fetchDetails(loginType, tables,loginId,password)
             if details:
-                return render_template('student.html',params=params,details=details,currentDate=datetime.now().date())
+                session['loginType'] = loginType
+                session['loginId'] = loginId
+                session['password'] = password
+                return render_template('student.html',params=params,details=details)
             else:
-                return render_template('index.html',params=params,style='d-block',errorMsg='The login id or password you entered is incorrect.')
-        elif loginType == '2':
-            details = fetchDetails(loginType,request.form['loginId'],request.form['password'])
+                return render_template('index.html',params=params,error="Please select the user Type or enter the correct user id or password")
+        elif loginType=='2':
+            details = fetchDetails(loginType, ['faculty_details'],loginId,password)
             if details:
-                print(json.loads(details[-1])[0])
-                return render_template('faculty.html',params=params,details=details,currentDate=datetime.now().date())
+                session['loginType'] = loginType
+                session['loginId'] = loginId
+                session['password'] = password
+                return render_template('faculty.html',params=params,details=details)
             else:
-                return render_template('index.html',params=params,style='d-block',errorMsg='The login id or password you entered is incorrect.')
-        else:
-            return render_template('index.html',params=params,style='d-block',errorMsg='Please select a login type')
-    return render_template('index.html',params=params,style='d-none',errorMsg='')
-
-
+                return render_template('index.html',params=params,error="Please select the user Type or enter the correct user id or password")
+    return render_template('index.html',params=params)
 @app.route('/about')
 def about():
     return render_template('about.html',params=params)
+@app.route('/logout')
+def logout():
+    session.clear()
+    return render_template('thanks-card.html',params=params,message="You've successfully logged out")
 @app.route('/signup',methods=['GET','POST'])
 def signUp():
+    mycursor.execute("SHOW TABLES")
+    tables = []
+    for table in mycursor:
+        if f"{datetime.now().year}" in table[0]:
+            tables.append(table[0])
     if request.method == "POST":
         fullName = request.form['firstName'] +" "+ request.form['lastName']
         email = request.form['email']
         password = request.form['password']
         confirm_password = request.form['confirm_password']
-        if checkPassword(confirm_password.encode("utf-8"),encrypt(password)):
-            userType = request.form['userType']
-            if userType == '1':
-                department = request.form['department']
-                semester = request.form['semester']
-                rollNumber = request.form['rollNumber']
-                image_file = request.form['image_file']
-                table_name = f"{department}_{semester}_{datetime.now().year}"
-                try:
-                    sql = f"CREATE TABLE {table_name} (`loginId` BIGINT NOT NULL , `name` TEXT NOT NULL , `department` VARCHAR(10) NOT NULL , `semester` VARCHAR(10) NOT NULL , `email` VARCHAR(50) NOT NULL , `password` VARCHAR(250) NOT NULL , `image_file` VARCHAR(100) NOT NULL )"
-                    mycursor.execute(sql)
-                except:
-                    pass
+        userType = request.form['userType']
+        if checkPassword(confirm_password.encode("utf-8"),encrypt(password)) and userType == '1':
+            department = request.form['department']
+            semester = request.form['semester']
+            rollNumber = request.form['rollNumber']
+            table_name = f"{department}_{semester}_{datetime.now().year}"
+            try:
+                sql = f"CREATE TABLE {table_name} (`loginId` BIGINT NOT NULL , `name` TEXT NOT NULL , `department` VARCHAR(10) NOT NULL , `semester` VARCHAR(10) NOT NULL , `email` VARCHAR(50) NOT NULL , `password` VARCHAR(250) NOT NULL , `image_file` VARCHAR(100) NOT NULL )"
+                mycursor.execute(sql)
+            except:
+                pass
                 
-                # Check whether roll no exists in a table or not
-                result = fetchDetails(userType, rollNumber,password)
+            # Check whether roll no exists in a table or not
+            result = None
+            for table in tables:
+                mycursor.execute(f"SELECT * FROM {table} WHERE loginId = %s",(rollNumber,))
+                result = mycursor.fetchone()
                 if result:
-                    return render_template('thanks-card.html',params=params,message="You've already registered")
-                else:
-                    sql = f"INSERT INTO {table_name} (`loginId`, `name`, `department`, `semester`, `email`, `password`, `image_file`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-                    password = encrypt(password)
-                    val = (rollNumber,fullName,department,semester,email, password,image_file)
+                    break
+            if result:
+                return render_template('thanks-card.html',params=params,message="You've already registered")
+            else:
+                sql = f"INSERT INTO {table_name} (`loginId`, `name`, `department`, `semester`, `email`, `password`, `image_file`) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+                password = encrypt(password)
+                f = request.files['image_file']
+                if f.content_type == 'image/jpeg':
+                    try:
+                        os.mkdir(os.path.join(app.config['UPLOAD_FOLDER']+f"/{table_name}"))
+                    except:
+                        pass
+                    f.save(os.path.join(app.config['UPLOAD_FOLDER']+f"/{table_name}",f"{rollNumber}.jpg"))
+                    val = (rollNumber,fullName,department,semester,email, password,f"{rollNumber}.jpg")
                     mycursor.execute(sql,val)
                     mydb.commit()
                     return render_template('thanks-card.html',params=params,message="You've registered successfully")
-                
-            elif userType == '2':
-                subject = request.form('subject')
-                table_name = f"{subject}_{datetime.now().year}"
-                print(table_name)
-                
-                # TODO
+        elif userType == '2' and checkPassword(confirm_password.encode("utf-8"),encrypt(password)):
+            result = None
+            for table in tables:
+                mycursor.execute(f"SELECT * FROM `faculty_details` WHERE loginId = %s",(email,))
+                result = mycursor.fetchone()
+                if result:
+                    break
+            if result:
+                return render_template('thanks-card.html',params=params,message="You've already registered")
             else:
-                return render_template('signup.html',params=params,style='d-block',errorMsg='You haven\'t chosen your role')
+                sql = f"INSERT INTO `faculty_details` (`name`, `loginId`, `password`) VALUES (%s, %s, %s)"
+                password = encrypt(password)
+                val = (fullName,email, password)
+                mycursor.execute(sql,val)
+                mydb.commit()
+                return render_template('thanks-card.html',params=params,message="You've registered successfully")
         else:
-            return render_template('signup.html',params=params,style='d-block',errorMsg='Your New password and confirmed password should be same')   
-    return render_template('signup.html',params=params,style='d-none',errorMsg='')
+            return render_template('signup.html',params=params,error="Please select your role")
+    return render_template('signup.html',params=params)
 @app.route('/contact',methods=['GET','POST'])
 def contact():
     if request.method == "POST":
@@ -125,5 +175,4 @@ def contact():
     else:
         return render_template('contact.html',params=params)
 if __name__ == '__main__':
-    
     app.run(debug=True)
